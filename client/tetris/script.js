9e5be767b2d1f2570007e9203e163327553e8a96
Mercy <mercy.akede@gmail.com>
2022-02-17 22:53:24 -0500

    Update Arrow Key Functionality

--- client/tetris/script.js
+++ client/tetris/script.js
@@ -66,6 +66,20 @@ document.addEventListener('DOMContentLoaded', () => {
   // make the tetrimino move down ever second
   timerId = setInterval(moveDown, 1000);
 
+  // assign functin to keyCodes
+  function control (e) {
+    if (e.keyCode === 37) {
+      moveLeft();
+    } else if (e.keyCode === 39) {
+      moveRight();
+    } else if (e.keyCode === 38) {
+      rotate();
+    } else if (e.keyCode === 40) {
+      moveDown();
+    }
+  }
+  document.addEventListener('keyup', control)
+
   // move down function
   function moveDown() {
     undraw();
@@ -84,5 +98,44 @@ document.addEventListener('DOMContentLoaded', () => {
       currentPosition = 4;
       draw();
     }
+  }
+
+  // move the tetrimino left, until there is an edge or a blockage
+
+  function moveLeft() {
+    undraw();
+    const isAtLeftEdge = current.some(index => (currentPosition + index) % 10 === 0);
+
+    if(!isAtLeftEdge) currentPosition -= 1;
+
+    if(current.some(index => squares[currentPosition + index].classList.contains('taken'))) {
+      currentPosition +=1;
+    }
+
+    draw();
+  }
+
+  function moveRight() {
+    undraw();
+    const isAtRightEdge = current.some(index => (currentPosition +index) % 10 === width-1);
+
+    if (!isAtRightEdge) currentPosition +=1;
+
+    if (current.some(index => squares[currentPosition + index].classList.contains('taken'))) {
+      currentPosition -= 1;
+    }
+    draw();
+  }
+
+  // rotate the tetrimino
+  function rotate() {
+    undraw();
+    currentRotation++;
+    if (currentRotation === current.length) { // if the current rotation gets to 4, loop back to 0
+      currentRotation = 0;
+    }
+
+    current = theTetriminos[random][currentRotation];
+    draw();
   }
 })
